978f3010bd78d2d473fcc0e9df4c171517766da2
Olivier Picard <32243829+olivierpicard@users.noreply.github.com>
2022-12-01 02:11:38 +0400

    Draw Presenter extract tool from ToolEvent

--- lib/presenter.dart
+++ lib/presenter.dart
@@ -1,16 +1,25 @@
+import 'package:grapher/kernel/propagator/endline.dart';
+import 'package:grapher/kernel/object.dart';
 import 'package:grapher/view/view-event.dart';
 import 'package:grapher_user_draw/draw_tools/draw_info.dart';
+import 'package:grapher_user_draw/draw_tools/draw_tool_event.dart';
 import 'package:grapher_user_draw/store.dart';
 
 import 'draw_tools/draw_tool_interface.dart';
 import 'figure.dart';
 
-class DrawPresenter {
-  final DrawToolInterface _tool;
+class DrawPresenter extends GraphObject with EndlinePropagator {
+  DrawToolInterface? _tool;
   final FigureStore _store;
   late DrawInfo drawInfo;
 
-  DrawPresenter(this._tool, this._store);
+  DrawPresenter(this._store) {
+    eventRegistry.add(DrawToolEvent, (p0) => _onDrawToolEvent(p0));
+  }
+
+  void _onDrawToolEvent(DrawToolEvent event) {
+    _tool = event.tool;
+  }
 
   void draw(ViewEvent event) {
     drawInfo = DrawInfo(event);
@@ -20,6 +29,6 @@ class DrawPresenter {
   }
 
   void _drawFigure(Figure figure) {
-    _tool.draw(drawInfo, figure);
+    _tool?.draw(drawInfo, figure);
   }
 }
--- test/presenter_test.dart
+++ test/presenter_test.dart
@@ -4,10 +4,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:grapher/filter/dataStruct/data2D.dart';
 import 'package:grapher/kernel/drawEvent.dart';
 import 'package:grapher/kernel/drawZone.dart';
+import 'package:grapher/kernel/object.dart';
+import 'package:grapher/kernel/propagator/single.dart';
 import 'package:grapher/view/axis/unit-axis.dart';
 import 'package:grapher/view/axis/virtual-axis.dart';
 import 'package:grapher/view/view-event.dart';
 import 'package:grapher_user_draw/draw_tools/draw_info.dart';
+import 'package:grapher_user_draw/draw_tools/draw_tool_event.dart';
 import 'package:grapher_user_draw/draw_tools/draw_tool_interface.dart';
 import 'package:grapher_user_draw/figure.dart';
 import 'package:grapher_user_draw/presenter.dart';
@@ -27,6 +30,16 @@ class MockVirtualAxis extends Mock implements VirtualAxis {}
 class MockDrawInfo extends Mock implements DrawInfo {}
 
 class MockStore extends Mock implements FigureStore {}
+
+class MockPropagator extends GraphObject with SinglePropagator {
+  MockPropagator(GraphObject child) {
+    this.child = child;
+  }
+
+  void propagateToolEvent(DrawToolEvent event) {
+    propagate(event);
+  }
+}
 
 void main() {
   final Canvas mockCanvas = MockCanvas();
@@ -48,11 +61,12 @@ void main() {
 
   setUp(() {
     mockTool = MockDrawTool();
-    presenter = DrawPresenter(mockTool, mockStore);
+    presenter = DrawPresenter(mockStore);
     toolDraw = () => mockTool.draw(captureAny(), captureAny());
   });
 
   test("Assert tool's draw function is called on presenter draw", () {
+    MockPropagator(presenter).propagate(DrawToolEvent(mockTool));
     when(() => mockStore.getAll()).thenReturn([mockFigure, MockFigure()]);
     presenter.draw(viewEvent);
 
