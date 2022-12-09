4b6e2572835d108010e75c56bff699d0d1faf872
Olivier Picard <32243829+olivierpicard@users.noreply.github.com>
2022-12-09 19:59:31 +0400

    Add more test to the pointer convertion logic These tests was originally issue from gestuer interpreter

--- test/gesture_interpreter_test.dart
+++ test/gesture_interpreter_test.dart
@@ -2,7 +2,6 @@ import 'dart:ui';
 
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
-// import 'package:grapher/kernel/drawZone.dart';
 import 'package:grapher/kernel/object.dart';
 import 'package:grapher/reference/reader.dart';
 import 'package:grapher_user_draw/bypass_pointer_event.dart';
@@ -49,7 +48,6 @@ class TestGestureInterpreter {
   late final InteractionReference _interactionRef;
   final _store = FigureStore();
   final outputVCoord = VirtualCoord(DateTime(2022, 11, 17, 15), 1495);
-  // final _safeDrawZone = DrawZone(const Offset(0, 0), const Size(10000, 10000));
   late final MockPointerConverterLogic _mockConverterLogic;
 
   TestGestureInterpreter() {
@@ -63,7 +61,6 @@ class TestGestureInterpreter {
         refGraphDragBlocker: MockReferenceReader(),
         interactionReference: _interactionRef,
         zoneConverter: _mockConverterLogic);
-    // _interpreter.updateDrawZone(_safeDrawZone);
 
     mockConvertionToVirtual() => _mockConverterLogic.toVirtual(any());
     when(() => mockConvertionToVirtual()).thenReturn(outputVCoord);
@@ -109,28 +106,6 @@ class TestGestureInterpreter {
       });
     });
   }
-
-  // void expectPointerOutOfDrawZoneIsIgnored() {
-  //   group('When pointer is out of the DrawZone', () {
-  //     const tapPos = Offset(100, 10);
-  //     // final drawZone = DrawZone(const Offset(0, 500), const Size(1000, 500));
-  //     // _interpreter.updateDrawZone(drawZone);
-  //     test('Expect UserInteraction\'s Tap is ignored', () {
-  //       _interpreter.onTapUp(TapUpDetails(
-  //         kind: PointerDeviceKind.unknown,
-  //         localPosition: tapPos,
-  //       ));
-  //       verifyNever(() => _interactionRef.tapInterface.onTap(any()));
-  //     });
-  //     test('Expect UserInteraction\'s Drag is ignored', () {
-  //       _interpreter.onTapUp(TapUpDetails(
-  //         kind: PointerDeviceKind.unknown,
-  //         localPosition: tapPos,
-  //       ));
-  //       verifyNever(() => _interactionRef.tapInterface.onTap(any()));
-  //     });
-  //   });
-  // }
 
   void assertDragStartIsCalledBeforeDrag() {
     test(
--- test/pointer_convetion_logic_test.dart
+++ test/pointer_convetion_logic_test.dart
@@ -12,32 +12,40 @@ class MockRect extends Mock implements Rect {}
 
 void main() {
   registerFallbackValue(Offset.zero);
-  const radomPixelPosition = Offset(10, 30);
+  const pointerInsideZone = Offset(11, 30);
+  const pointerOutsideZone = Offset(200, 30);
   final expectedVirtualCoord = VirtualCoord(DateTime(2022, 12, 09), 12);
-  final mockZonePointable = MockRect();
+  final zonePointable = const Offset(10, 10) & const Size(100, 100);
   final mockCoordTranslater = MockCoordTranslater();
   late PointerConvertionLogic converter;
 
   setUp(() {
     converter = PointerConvertionLogic();
-    when(() => mockZonePointable.contains(any())).thenReturn(true);
     when(() => mockCoordTranslater.toVirtual(any()))
         .thenReturn(expectedVirtualCoord);
   });
 
   test("Check convertion from pixel to virtual coord", () {
-    converter.refresh(mockCoordTranslater, mockZonePointable);
-    final resultingVirtualCoord = converter.toVirtual(radomPixelPosition);
+    converter.refresh(mockCoordTranslater, zonePointable);
+    final resultingVirtualCoord = converter.toVirtual(pointerInsideZone);
     expect(resultingVirtualCoord, equals(expectedVirtualCoord));
   });
 
   group("Assert StateError is thrown during pointer convertion when", () {
     test("zone pointable is null", () {
       converter.refresh(mockCoordTranslater, null);
-      expect(() => converter.toVirtual(radomPixelPosition), throwsStateError);
+      expect(() => converter.toVirtual(pointerInsideZone), throwsStateError);
     });
     test("when refresh is not called", () {
-      expect(() => converter.toVirtual(radomPixelPosition), throwsStateError);
+      expect(() => converter.toVirtual(pointerInsideZone), throwsStateError);
+    });
+  });
+
+  group('When pointer is out of the pointable zone', () {
+    test('expect the convertion to virtual return null', () {
+      converter.refresh(mockCoordTranslater, zonePointable);
+      final resultingVirtualCoord = converter.toVirtual(pointerOutsideZone);
+      expect(resultingVirtualCoord, isNull);
     });
   });
 }
