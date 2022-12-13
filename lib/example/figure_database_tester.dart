22fd4d6b688ff461eab910446292d483e8e95725
Olivier Picard <32243829+olivierpicard@users.noreply.github.com>
2022-12-13 22:19:35 +0400

    Add a Database tester to check load function at start

--- lib/example/figure_database_tester.dart
+++ lib/example/figure_database_tester.dart
@@ -1,8 +1,22 @@
+import 'package:grapher_user_draw/anchor.dart';
+import 'package:grapher_user_draw/example/draw_tool_tester.dart';
 import 'package:grapher_user_draw/figure.dart';
 import 'package:grapher_user_draw/figure_database_interface.dart';
 
 class FigureDatabaseTester implements FigureDatabaseInterface {
+  final defaultFigure = Figure(DrawToolTester());
   var figures = <Figure>[];
+
+  FigureDatabaseTester() {
+    defaultFigure
+        .add(Anchor(x: DateTime.utc(2021, 01, 28, 10, 04, 00), y: 1.211065));
+    defaultFigure
+        .add(Anchor(x: DateTime.utc(2021, 01, 28, 10, 04, 20), y: 1.211061));
+    defaultFigure
+        .add(Anchor(x: DateTime.utc(2021, 01, 28, 10, 04, 55), y: 1.211059));
+    figures.add(defaultFigure);
+  }
+
   @override
   List<Figure> load() {
     return figures;
